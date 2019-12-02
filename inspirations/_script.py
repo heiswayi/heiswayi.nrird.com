2c663582b6c8ce90f7b6a2da214e49613477080b
Heiswayi Nrird <heiswayi@nrird.com>
2019-12-03 00:43:37 +0800

    Add inspirations section

---
+++ inspirations/_script.py
@@ -0,0 +1,24 @@
+import os
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
+
+listOfFiles = getListOfFiles("./")
+for file in listOfFiles:
+	if not file.endswith(".py"):
+		s = '"' + file.replace("./", "") + '",'
+		print(s)
