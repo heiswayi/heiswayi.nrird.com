36635aab23340b57ee00208d6e7be6deb64cc413
Heiswayi Nrird <hnrird@gmail.com>
2022-08-28 22:47:38 +0800

    Fixed build error

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const sass = require('gulp-sass')(require('sass'));
 const prefixer = require('gulp-autoprefixer');
 const minify = require('gulp-clean-css');
 const rename = require('gulp-rename');
-const shell = require('gulp-shell');
 
 const destDir = '_site/assets/css';
 const scssDir = [
